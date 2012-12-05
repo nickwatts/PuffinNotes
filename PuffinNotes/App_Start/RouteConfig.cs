2f6a1b5c69a2b17afeb620ac982671a87c597fae
Tim Butler <timothygbutler@gmail.com>
2012-12-05 18:19:34 +0000

    added default controller and view

--- PuffinNotes/App_Start/RouteConfig.cs
+++ PuffinNotes/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace PuffinNotes
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Default", action = "index", id = UrlParameter.Optional }
             );
         }
     }
---
+++ PuffinNotes/Controllers/DefaultController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PuffinNotes.Controllers
+{
+    public class DefaultController : Controller
+    {
+        //
+        // GET: /Default/
+
+        public ActionResult index()
+        {
+            return View();
+        }
+
+    }
+}
