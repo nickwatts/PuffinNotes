23a2c13d9d2b580457ec3831a26e5d52636efd33
nickwatts <nickwatts80@googlemail.com>
2013-03-04 18:01:50 +0000

    Made notes on /noticeboard/details/x jqueryui draggable

---
+++ PuffinNotes/Scripts/AddJqueryUIControls.js
@@ -0,0 +1,4 @@
+﻿//Sets controls with a class of DraggableListItem as draggable using JQueryUI
+$(function () {
+    $(".DraggableListItem").draggable({ containment: ".NoticeBoard", scroll: false });
+});
