7e4439003a8fcc4dfb41e9156ca85bcd9e85b56a
nickwatts <nickwatts80@googlemail.com>
2013-03-13 19:41:38 +0000

    Bug fix for content being overwritten when moving a note

--- PuffinNotes/Scripts/Custom.js
+++ PuffinNotes/Scripts/Custom.js
@@ -1,15 +1,16 @@
-﻿function setlocation(id)
+﻿function setlocation(boardID, id, author, content)
 { 
     var note = document.getElementById(id);
     //alert("left = " + note.offsetLeft + " top = " + note.offsetTop);
     alert("at function");
-    var xcoordinate = note.offsetLeft;
-    var ycoordinate = note.offsetTop;
+    var xCoordinate = note.offsetLeft;
+    var yCoordinate = note.offsetTop;
+    var zCoordinate = note.offsetTop;
     $.ajax(
         {
             url: "/Note/EditPosition",
             type: "POST",
-            data: { 'noteID': id, 'xcoordinate': xcoordinate, 'ycoordinate': ycoordinate, 'boardid': 1},
-            success: function () { alert('xcoordinate = ' + xcoordinate); }
+            data: { 'boardID': boardID, 'noteID': id, 'author': author, 'content': content, 'xCoordinate': xCoordinate, 'yCoordinate': yCoordinate, 'zCoordinate': zCoordinate},
+            success: function () { }
             });
 }
