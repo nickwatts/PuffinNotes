c3526ad3c2c4e63d98955191ed99274c021b8a00
Tim Butler <timothygbutler@gmail.com>
2013-07-23 20:42:57 +0100

    Removed jquery nuget package and fixed z-index

--- PuffinNotes/Scripts/Custom.js
+++ PuffinNotes/Scripts/Custom.js
@@ -1,16 +1,34 @@
-﻿function setlocation(boardID, id, author, content)
+﻿function setlocation(boardID, author, content)
 { 
-    var note = document.getElementById(id);
-    //alert("left = " + note.offsetLeft + " top = " + note.offsetTop);
-    //alert("at function");
-    var xCoordinate = note.offsetLeft;
-    var yCoordinate = note.offsetTop;
-    var zCoordinate = note.offsetTop;
+
+    var allnotes = $('.note');
+
+
+    for (var i = 0; i < allnotes.length; i++)
+    {
+        var note = allnotes[i];
+
+        var xCoordinate = note.offsetLeft;
+        var yCoordinate = note.offsetTop;
+        var zCoordinate = parseInt(note.style.zIndex); // + 1;//$(note).css('zIndex');
+       
+        //note.innerText = note.innerText + " Zindex= " + note.style.zIndex;
     $.ajax(
         {
             url: "/Note/EditPosition",
             type: "POST",
-            data: { 'boardID': boardID, 'noteID': id, 'author': author, 'content': content, 'xCoordinate': xCoordinate, 'yCoordinate': yCoordinate, 'zCoordinate': zCoordinate},
+            data: { 'boardID': boardID, 'noteID': note.id, 'author': author, 'content': note.innerText, 'xCoordinate': xCoordinate, 'yCoordinate': yCoordinate, 'zCoordinate': zCoordinate},
             success: function () { }
             });
+
+    }
+
+
+}
+
+
+function getElements(attrib)
+{
+
+    return $('[' + attrib + ']').get();
 }
